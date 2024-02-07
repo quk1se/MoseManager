b7e80d11d50ccdce3da37ee80b18aef99832bfbc
alexandr <KalX_si54@student.itstep.org>
2024-02-07 22:55:23 +0200

    navbar profile and dashboard

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+var account_field = document.getElementById("account-field");
+var full_profile = document.getElementById("full-profile");
+var open_account_field = false;
+function ShowProfile()
+{
+    if (!open_account_field)
+    {
+        full_profile.style.display = "flex";
+        account_field.style.height = "30%";
+        open_account_field = true;
+        setTimeout(function() {
+            full_profile.style.opacity = "1";
+        }, 100);
+    }
+    else
+    {
+        account_field.style.height = "10%";
+        open_account_field = false;
+        full_profile.style.opacity = "0";
+        setTimeout(function() {
+            full_profile.style.display = "none";
+        }, 200);
+    }
+}
